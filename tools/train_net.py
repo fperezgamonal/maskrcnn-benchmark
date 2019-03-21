1a684264b6379f093815ffa1dd51642aa3c6110a
Ferran Pérez Gamonal <fperez.gamonal@gmail.com>
2019-03-21 11:45:28 +0000

    yet more syncing local changes with remote (problems with GoogleColab)

--- tools/train_net.py
+++ tools/train_net.py
@@ -50,7 +50,7 @@ def train(cfg, local_rank, distributed):
     checkpointer = DetectronCheckpointer(
         cfg, model, optimizer, scheduler, output_dir, save_to_disk
     )
-    extra_checkpoint_data = checkpointer.load(cfg.MODEL.WEIGHT)
+    extra_checkpoint_data = checkpointer.load(cfg.MODEL.WEIGHT, init=True)
     arguments.update(extra_checkpoint_data)
 
     data_loader = make_data_loader(
